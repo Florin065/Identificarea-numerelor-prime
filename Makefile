a67062c76bb2452ca978732375c7ab0aec58ea7a
FLO777 <subtirica.florin29@gmail.com>
2022-12-22 03:25:48 +0200

    new programs for only one test

--- Makefile
+++ Makefile
@@ -1,28 +1,41 @@
 CC = gcc
+CFLAG = -o
 
 .PHONY: build
 build:
-	$(CC) Fermat.c -o Fermat
-	$(CC) Rabin.c -o Rabin
+	$(CC) Fermat.c $(CFLAG) Fermat
+	$(CC) Rabin.c $(CFLAG) Rabin
+	$(CC) FermatTest.c $(CFLAG) FermatTest
+	$(CC) RabinTest.c $(CFLAG) RabinTest
 
-all: run-best run-p1 run-p2
+all: run-all-best run-all-p1 run-all-p2
 
-run-best: Rabin.o
-	./Rabin > run-best.txt
-	./rabin.sh > run-best-accuracy.txt
+test: run-best run-p1 run-p2
 
-run-p1: Fermat.o
-	./Fermat > run-p1.txt
-	./fermat.sh > run-p1-accuracy.txt
+run-best: RabinTest
+	./RabinTest > run-test-best.txt
 
-run-p2: Rabin.o
-	./Rabin > run-p2.txt
-	./rabin.sh > run-p2-accuracy.txt
+run-p1: FermatTest
+	./FermatTest > run-test-p1.txt
+
+run-p2: RabinTest
+	./RabinTest > run-test-p2.txt
+
+run-all-best: Rabin
+	./Rabin > run-all-best.txt
+	./rabin.sh > run-all-best-accuracy.txt
+
+run-all-p1: Fermat
+	./Fermat > run-all-p1.txt
+	./fermat.sh > run-all-p1-accuracy.txt
+
+run-all-p2: Rabin
+	./Rabin > run-all-p2.txt
+	./rabin.sh > run-all-p2-accuracy.txt
 
 .PHONY: clean
 clean:
-	-rm -rf outRabin/
-	-rm -rf outFermat/
-	-rm -rf Rabin Rabin.o
-	-rm -rf Fermat Fermat.o
-	-rm -rf run-best.txt run-p1.txt run-p2.txt run-best-accuracy.txt run-p1-accuracy.txt run-p2-accuracy.txt
+	-rm -rf outRabin/ outFermat/
+	-rm -rf Rabin RabinTest Fermat FermatTest
+	-rm -rf run-all-best.txt run-all-p1.txt run-all-p2.txt run-test-best.txt run-test-p1.txt run-test-p2.txt
+	-rm -rf run-all-best-accuracy.txt run-all-p1-accuracy.txt run-all-p2-accuracy.txt
---
+++ RabinTest.c
@@ -0,0 +1,172 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+// Utility function to do modular exponentiation.
+// It returns (x^y) % p
+int power(int x, size_t y, int p)
+{
+    int res = 1;      // Initialize result
+    x %= p;  // Update x if it is more than or
+                // equal to p
+    while (y > 0)
+    {
+        // If y is odd, multiply x with result
+        if (y & 1) {
+            res = (res * x) % p;
+        }
+ 
+        // y must be even now
+        y = y >> 1; // y = y/2
+        x = (x * x) % p;
+    }
+    return res;
+}
+ 
+// This function is called for all k trials. It returns
+// false if n is composite and returns true if n is
+// probably prime.
+// d is an odd number such that  d*2 = n-1
+// for some r >= 1
+bool millerTest(int d, int n)
+{
+    // Pick a random number in [2..n-2]
+    // Corner cases make sure that n > 4
+    int a = 2 + rand() % (n - 4);
+ 
+    // Compute a^d % n
+    int x = power(a, d, n);
+ 
+    if (x == 1  || x == n-1)
+       return true;
+ 
+    // Keep squaring x while one of the following doesn't
+    // happen
+    // (i)   d does not reach n-1
+    // (ii)  (x^2) % n is not 1
+    // (iii) (x^2) % n is not n-1
+    while (d != n - 1)
+    {
+        x = (x * x) % n;
+        d *= 2;
+ 
+        if (x == 1) {
+            return false;
+        }
+        if (x == n - 1) {
+            return true;
+        }
+    }
+ 
+    // Return composite
+    return false;
+}
+ 
+// It returns false if n is composite and returns true if n
+// is probably prime.  k is an input parameter that determines
+// accuracy level. Higher value of k indicates more accuracy.
+bool isPrime(int n, int k)
+{
+    // Corner cases
+    if (n <= 1 || n == 4) {
+        return false;
+    }
+
+    if (n <= 3) {
+        return true;
+    }
+ 
+    // Find r such that n = 2^d * r + 1 for some r >= 1
+    int d = n - 1;
+    while (d % 2 == 0) {
+        d /= 2;
+    }
+ 
+    // Iterate given number of 'k' times
+    for (int i = 0; i < k; i++) {
+                 if (!millerTest(d, n)) {
+              return false;
+         }
+    }
+ 
+    return true;
+}
+
+int main() {
+    // Create file name strings
+    char inFileName[256];
+    strcpy(inFileName, "test.in");
+    char outFileName[256];
+    strcpy(outFileName, "test.out");
+    
+    // Open the files
+    FILE *inFilePtr = fopen(inFileName, "r");
+    FILE *outFilePtr = fopen(outFileName, "w");
+    
+    // Get the number of elements in the sequence
+    int N;
+    fscanf(inFilePtr, "%d\n", &N);
+    
+    // Create an array to store the sequence
+    int seq[N];
+    
+    // Parse the sequence
+    for (int i = 0; i < N; i++) {
+        fscanf(inFilePtr, "%d", &seq[i]);
+    }
+
+    // Get the start time
+    struct timeval startTime;
+    gettimeofday(&startTime, NULL);
+    
+    // Count the number of prime elements in the sequence
+    int M = 0;
+    for (int i = 0; i < N; i++) {
+        if (isPrime(seq[i], 10)) {
+            M++;
+        }
+    }
+
+    // Get the end time
+    struct timeval endTime;
+    gettimeofday(&endTime, NULL);
+    
+    // Get the time difference in milliseconds
+    double timeInMilliseconds = 
+        (endTime.tv_sec - startTime.tv_sec) * 1000.0 + 
+        (endTime.tv_usec - startTime.tv_usec) / 1000.0;
+    
+    // Get the memory used
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    double memoryInKilobytes = (double)usage.ru_maxrss / 1024;
+    
+    // Print the time and memory used
+    printf(inFileName);
+    printf("Time: %fms\n", timeInMilliseconds);
+    printf("Memory: %fkB\n", memoryInKilobytes);
+    
+    // Print the number of prime elements
+    fprintf(outFilePtr, "%d\n", M);
+    
+    // Print the prime elements
+    for (int i = 0; i < N; i++) {
+        if (isPrime(seq[i], 10)) {
+            fprintf(outFilePtr, "%d ", seq[i]);
+        }
+    }
+    fprintf(outFilePtr, "\n");
+    
+    // Close the files
+    fclose(inFilePtr);
+    fclose(outFilePtr);
+    
+    return 0;
+}
